554923c5ee6c088d0aa5ee11b36b98e9ffde09ee
Dave Monahan <dmonahan24@gmail.com>
2021-08-05 17:33:06 -0400

    troubleshooting webcam screenshot

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import appReducer from '../features/appSlice';
-import cameraReducer from '../features/appSlice';
+import cameraReducer from '../features/cameraSlice';
 
 export const store = configureStore({
   reducer: {
